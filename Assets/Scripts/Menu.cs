335d060d0108b84d169c3955733e62ad1415a829
altuxast <55614975+altuxast@users.noreply.github.com>
2024-03-15 09:53:35 +1100

    Completed pause menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,10 +5,54 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public void StartGame(){
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public void Start()
+    {
+        if (this.name == "PauseMenuCanvas")
+        {
+            Transform pauseMenuPanel = transform.Find("Panel_PauseMenu");
+            Transform resumeGameBtn = pauseMenuPanel.transform.Find("PauseMenuItemResumeGame_Btn");
+            if(resumeGameBtn != null){
+                Debug.Log(resumeGameBtn.GetComponent<UI>());
+            }
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void StartGame()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    public void QuitGame(){
+    public void QuitGame()
+    {
         Application.Quit();
+    }
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 }
