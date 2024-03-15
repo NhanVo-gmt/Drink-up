d15ae3208d2aeaa393471d0b48c872de36c557c5
altuxast <55614975+altuxast@users.noreply.github.com>
2024-03-15 21:54:27 +1100

    Completed lives system
    Button deactivates a single life with each click.

--- Assets/Scripts/LifeSystem.cs
+++ Assets/Scripts/LifeSystem.cs
@@ -7,37 +7,46 @@ public class LifeSystem : MonoBehaviour
     public GameObject[] lives;
     public int life;
 
-    void start()
+    void Start()
     {
-        // foreach(GameObject life in lives)
+        // foreach (GameObject life in lives)
         // {
         //     Debug.Log(life.name);
         // }
-        Debug.Log(this.name);
+        // Debug.Log(this.name);
     }
 
     void Update()
     {
-        // if (life < 1)
-        // {
-        //     lives[0].gameObject.SetActive(false);
-        // }
-        // else if (life < 2)
-        // {
-        //     lives[1].gameObject.SetActive(false);
-        // }
-        // else if (life < 3)
-        // {
-        //     lives[2].gameObject.SetActive(false);
-        // }
-        // else if (life < 4)
-        // {
-        //     lives[3].gameObject.SetActive(false);
-        // }
-        // else if (life < 5)
-        // {
-        //     lives[4].gameObject.SetActive(false);
-        // }
+        if (life < 0)
+        {
+            life = 0;
+            for (int i = 0; i < lives.Length; i++)
+            {                
+                lives[i].gameObject.SetActive(true);
+                life++;
+            }
+        }
+        else if (life < 1)
+        {
+            lives[0].gameObject.SetActive(false);
+        }
+        else if (life < 2)
+        {
+            lives[1].gameObject.SetActive(false);
+        }
+        else if (life < 3)
+        {
+            lives[2].gameObject.SetActive(false);
+        }
+        else if (life < 4)
+        {
+            lives[3].gameObject.SetActive(false);
+        }
+        else if (life < 5)
+        {
+            lives[4].gameObject.SetActive(false);
+        }
     }
 
     public void TakeDamage(int damage)
