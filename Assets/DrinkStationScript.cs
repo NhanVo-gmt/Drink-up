4229b71a2bf33fba44ef6c9fbcc1a5561fda82d5
gnarley2 <112232940+gnarley2@users.noreply.github.com>
2024-03-15 17:35:13 +1100

    When Player is colliding with the drink-station-volume they can select their drink

---
+++ Assets/DrinkStationScript.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+
+public class DrinkStationScript : MonoBehaviour
+{
+    [SerializeField] public bool isPlayerInsideVolume; // Flag to check if the player is inside the DrinkStationVolume
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPlayerInsideVolume = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerInsideVolume)
+        {
+            SelectDrink();
+        }
+    }
+
+    void SelectDrink()
+    {
+        // Check for input to select items
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            // Select item 1
+
+            Debug.Log("Selected Drink: 1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            // Select item 2
+
+            Debug.Log("Selected Drink: 2");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            // Select item 3
+
+            Debug.Log("Selected Drink: 3");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            // Select item 4
+            Debug.Log("Selected Drink: 4");
+            
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+            isPlayerInsideVolume = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isPlayerInsideVolume = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+            isPlayerInsideVolume = false;
+    }
+}
+
+
