356f9a5b17e1817080feb26f3ea91b71270d7a9c
gnarley2 <112232940+gnarley2@users.noreply.github.com>
2024-03-17 17:35:58 +1100

    spawner randomizer

--- Assets/Scripts/NPCSpawner.cs
+++ Assets/Scripts/NPCSpawner.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class NPCSpawner : MonoBehaviour
 {
@@ -12,24 +14,39 @@ public class NPCSpawner : MonoBehaviour
 
     private float spawnElapseTime;
 
+
+
+    bool hasSpawned = false;
+    private float randomNumber;
+
     private void Awake()
     {
-        
+        randomNumber = Random.Range(1.20f, 3.00f);
     }
 
     private void Update()
     {
         Spawn();
+      Awake();
     }
 
     void Spawn()
     {
-        spawnElapseTime += Time.deltaTime;
-        if (spawnElapseTime >= spawnRate)
-        {
-            spawnElapseTime = 0;
 
+      
+        spawnElapseTime += Time.deltaTime;
+        if ((spawnElapseTime >= spawnRate) && !hasSpawned)
+        {
+             spawnElapseTime = 0;
+            hasSpawned = true;
             Instantiate(prefab, transform.position, quaternion.identity);
+        }
+
+        if (hasSpawned && spawnElapseTime >= (spawnRate*randomNumber))
+        {
+            Instantiate(prefab, transform.position, quaternion.identity);
+            spawnElapseTime = 0;
+            hasSpawned = false;
         }
     }
 }
