ae5569640d9fe506f5cc971d99bfb7159804f597
gnarley2 <112232940+gnarley2@users.noreply.github.com>
2024-03-17 15:09:47 +1100

    clink sound effect added

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static Unity.VisualScripting.Member;
 
 public class ScoreSystem : MonoBehaviour
 {
     public GameObject scoreUI;
     private Text m_scoreText;
     private int m_currentScore;
+    [SerializeField]private AudioSource audioSource3;
 
     void Start()
     {
         m_currentScore = 0;
         m_scoreText = scoreUI.GetComponent<Text>();
+        // audioSource3 = this.GetComponent<AudioSource>();
+        audioSource3 = GameObject.FindGameObjectWithTag("Score").GetComponent<AudioSource>();
     }
 
     public void AddPoints(int points)
@@ -20,6 +24,7 @@ public class ScoreSystem : MonoBehaviour
         m_currentScore += points;
         m_scoreText.text = "";
         m_scoreText.text = m_currentScore.ToString();
+        audioSource3.Play();
     }
 
     public void ResetScore()
